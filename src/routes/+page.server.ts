391dff6940e93f436f3e22097d0ce678ff8436f5
nooglersoon <nooglersoon@outlook.co.id>
2024-06-11 18:12:40 +0700

    change to dynamic

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { MAPBOX_API_KEY } from '$env/static/private'
+import { MAPBOX_API_KEY } from '$env/dynamic/private'
 
 export async function load() {
   return { MAPBOX_API_KEY }
